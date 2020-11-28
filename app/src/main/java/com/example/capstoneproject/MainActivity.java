25bae9d1b27db6f8bf9ab721b99826ea5cae4161
DythaAW <dytha.ananda24@gmail.com>
2020-11-28 19:01:59 +0700

    Tampilan

---
+++ app/src/main/java/com/example/capstoneproject/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.capstoneproject;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
