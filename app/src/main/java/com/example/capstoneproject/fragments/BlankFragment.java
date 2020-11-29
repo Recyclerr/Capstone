34b9e8e5474cc7d9e2b8da65f19645c5d7e1e25d
Ahmad Hafizh Zahran <zahran.hafizh@yahoo.co.id>
2020-11-29 17:27:37 +0700

    ALARM

--- app/src/main/java/com/example/capstoneproject/fragments/BlankFragment.java
+++ app/src/main/java/com/example/capstoneproject/fragments/BlankFragment.java
@@ -1,18 +1,33 @@
 package com.example.capstoneproject.fragments;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.example.capstoneproject.AlarmReceiver;
 import com.example.capstoneproject.LoginActivity;
 import com.example.capstoneproject.MainActivity;
 import com.example.capstoneproject.R;
+
+import static android.content.Context.ALARM_SERVICE;
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 
 public class BlankFragment extends Fragment {
@@ -22,6 +37,11 @@ public class BlankFragment extends Fragment {
 
     SharedPreferences sharedpreferences;
     Intent intent;
+    private static final int NOTIFICATION_ID = 0;
+    // Notification channel ID.
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+    private NotificationManager mNotificationManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,6 +55,71 @@ public class BlankFragment extends Fragment {
                 logout();
             }
         });
+
+        mNotificationManager = (NotificationManager)
+                getActivity().getSystemService(NOTIFICATION_SERVICE);
+
+        ToggleButton alarmToggle = view.findViewById(R.id.alarmToggle);
+
+        // Set up the Notification Broadcast Intent.
+        Intent notifyIntent = new Intent(getActivity(), AlarmReceiver.class);
+
+        boolean alarmUp = (PendingIntent.getBroadcast(getActivity(), NOTIFICATION_ID,
+                notifyIntent, PendingIntent.FLAG_NO_CREATE) != null);
+        alarmToggle.setChecked(alarmUp);
+
+        final PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
+                (getActivity(), NOTIFICATION_ID, notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService
+                (ALARM_SERVICE);
+
+        // Set the click listener for the toggle button.
+        alarmToggle.setOnCheckedChangeListener
+                (new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged
+                            (CompoundButton buttonView, boolean isChecked) {
+                        String toastMessage;
+                        if (isChecked) {
+
+                            long repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+
+                            long triggerTime = SystemClock.elapsedRealtime()
+                                    + repeatInterval;
+
+                            // If the Toggle is turned on, set the repeating alarm with
+                            // a 15 minute interval.
+                            if (alarmManager != null) {
+                                alarmManager.setInexactRepeating
+                                        (AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                                triggerTime, repeatInterval,
+                                                notifyPendingIntent);
+                            }
+                            // Set the toast message for the "on" case.
+                            toastMessage = getString(R.string.alarm_on_toast);
+
+                        } else {
+                            // Cancel notification if the alarm is turned off.
+                            mNotificationManager.cancelAll();
+
+                            if (alarmManager != null) {
+                                alarmManager.cancel(notifyPendingIntent);
+                            }
+                            // Set the toast message for the "off" case.
+                            toastMessage = getString(R.string.alarm_off_toast);
+
+                        }
+
+                        // Show a toast to say the alarm is turned on or off.
+                        Toast.makeText(getActivity(), toastMessage,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Create the notification channel.
+        createNotificationChannel();
         return view;
     }
 
@@ -53,6 +138,32 @@ public class BlankFragment extends Fragment {
         intent = new Intent(getActivity(), LoginActivity.class);
         getActivity().finish();
         startActivity(intent);
+    }
+
+    public void createNotificationChannel() {
+
+        // Create a notification manager object.
+        mNotificationManager =
+                (NotificationManager) getActivity().getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to " +
+                    "stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 
 }
